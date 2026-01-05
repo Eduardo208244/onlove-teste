f66fdb2df6fefa33aec8cc26942408f889287dc7
eduardo208244 <duduleite1701@gmail.com>
2026-01-04 22:25:04 -0300

    klklk

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,35 @@ import Link from "next/link";
 
 export default function Page() {
   return (
-    <div>
-      <h2>hello world</h2>
-      <Link href={"/create"}>Link</Link>
-    </div>
+    <main className="w-full m-0 flex flex-col">
+      <section className="w-full bg-red-700 h-150 p-4 flex flex-col items-center">
+        <div className="bg-white rounded-3xl w-[80%] p-4 m-auto flex flex-row justify-between items-center">
+          <div>
+            <h2>
+              OnLoveYou
+            </h2>
+          </div>
+          <div className="gap-5 flex flex-row">
+            <a href="#">Exemplo</a>
+            <a href="#">Preços</a>
+            <a href="#">Perguntas</a>
+          </div>
+          <div>
+            <Link href="/create">Criar Agora</Link>
+          </div>
+        </div>
+       <div>
+         <h1 className="text-5xl">Arte do Romance</h1>
+        <p>"Diga 'eu te amo' de um jeito diferente. ✨" Crie um site personalizado para o seu relacionamento. Adicione sua música favorita, fotos e uma carta de amor. O presente que cabe no seu bolso e no coração de quem você ama.</p>
+        <div>
+          <Link href="">Crie sua Lovepage</Link>
+          <Link href="">Exemplo</Link>
+        </div>
+        </div>
+      </section>      
+      <section></section>
+      <section></section>
+      <section></section>
+    </main>
   );
 }
